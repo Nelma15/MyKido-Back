35fc6704b3cdd591f163526b203731717e17d7f9
Manel Merini <myloviche15@gmail.com>
2025-03-10 00:28:20 +0100

    Adding colors to the command prompt

--- src/main/java/fr/MyKido/repository/DocumentsRepository.java
+++ src/main/java/fr/MyKido/repository/DocumentsRepository.java
@@ -1,9 +1,10 @@
 package fr.MyKido.repository;
 
-import javax.swing.text.Document;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DocumentsRepository extends JpaRepository<Document,Integer> {
+import fr.MyKido.models.Documents;
+
+public interface DocumentsRepository extends JpaRepository<Documents,Integer> {
 
 }
